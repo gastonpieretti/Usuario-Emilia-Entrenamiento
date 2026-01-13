09245224b33b4ecc05cc3cff7e9752873913258c
gastonpieretti <gastonpieretti@gmail.com>
2026-01-13 17:50:28 -0300

    Update userRoutes.ts

--- backend/src/routes/userRoutes.ts
+++ backend/src/routes/userRoutes.ts
@@ -5,10 +5,8 @@ import { PrismaClient } from '@prisma/client';
 const router = Router();
 const prisma = new PrismaClient();
 
-// Ruta para GUARDAR el perfil
 router.put('/profile/:id', updateProfile);
 
-// Ruta para VER el perfil (para que no le aparezca vacío al usuario)
 router.get('/profile/:id', async (req, res) => {
   try {
     const profile = await prisma.userProfile.findUnique({
